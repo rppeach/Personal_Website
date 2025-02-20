1fc49cbad520c2fde90612a238b64487b8895937
ee-rpeach <157056610+ee-rpeach@users.noreply.github.com>
2025-02-19 23:30:50 -0600

    Updated for Part 2

--- javascript.js
+++ javascript.js
@@ -1,2 +1,11 @@
 // I need to put something like a hello world script here but I'm just gonna let you know what's going on in case you think I'm trying to take credit for amazon and facebook
-alert(`If you check out my portfolio page, jsut know the grocery list is mine, the other sites are not of my design and I do not take credit for them`)
+alert(`If you check out my portfolio page, just know the grocery list is mine, the other sites are not of my design and I do not take credit for them`)
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
---
+++ success.js
@@ -0,0 +1,4 @@
+alert (`You Did It! You Pressed the button!`)
+setTimeout(() => {
+    window.location.href = window.location.origin
+  }, 5000)
